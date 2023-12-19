92c8c48b5d0438f2091d6f2d09cbab20227a5919
bamboo_chen <b10902005@ntu.edu.tw>
2023-12-19 22:19:31 +0800

    [MODIFY] news.py train.json

--- news.py
+++ news.py
@@ -11,8 +11,8 @@ page_num = 10
 seq_len = 1024
 #請求網站
 news_list = []
+cnt = 0
 for comp in stock_name:
-    cnt = 0
     for page in range(page_num):
         # 要抓取的網址
         url = 'https://tw.finance.yahoo.com/news_search.html?ei=Big5&q=' + quote(comp.encode('big5hkscs')) + f'&pg={page+1}'
@@ -25,56 +25,29 @@ for comp in stock_name:
         #將整個網站的程式碼爬下來
 
         soup = BeautifulSoup(list_req.content, "html.parser")
-
         get_all_news = soup.find('table', { 'id': 'newListTable' })
-
         single_news_tag_array = get_all_news.find_all('table')
-
         # get all news title
-
         all_news_title = []
-
         for news in single_news_tag_array:
-
             title_tags = news.find_all('a', { 'class': 'mbody' })
-
             if len(title_tags) >= 2:
-
                 title = title_tags[-1].get_text()
-
                 all_news_title.append(title)
 
-        # get all news body
-
         all_news_body = []
-
         for news in single_news_tag_array:
-
             body = news.find_all('span', { 'class': 'mbody' })
-
             if len(body) >= 2:
-
                 all_news_body.append(body[0].get_text())
 
-                
-
-        # get all links to news
-
         all_news_links = []
-
         for news in single_news_tag_array:
-
             a_tags = news.find_all('span', { 'class': 'mbody' })
-
             for tag in a_tags:
-
                 link = tag.find('a')
-
                 if link != None:
-
                     all_news_links.append(link['href'])
-
-        # print to console
 
         for i in range(len(all_news_title)):
 
